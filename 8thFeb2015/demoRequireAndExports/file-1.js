8375d59bfb3e30b2acd35e605f8b7fd884f95cec
mtaha <stallion049@yahoo.com>
2015-02-08 23:48:10 +0500

    Demo sync code, usage of exports and require method

---
+++ 8thFeb2015/demoRequireAndExports/file-1.js
@@ -0,0 +1,16 @@
+/**
+ * Created by MTA on 2/8/2015.
+ */
+
+
+/*
+*
+*
+*
+* */
+
+var file2Ref = require("./file-2");
+
+//console.log("file2Ref");
+//console.log(file2Ref);
+file2Ref.myCustomFn();
---
+++ 8thFeb2015/demoRequireAndExports/file-2.js
@@ -0,0 +1,20 @@
+/**
+ * Created by MTA on 2/8/2015.
+ */
+
+
+function doSomething(){
+    console.log("I am ordered to do something");
+}
+
+
+exports.myCustomFn = doSomething;
+
+//doSomething();
+
+
+
+
+
+
+
